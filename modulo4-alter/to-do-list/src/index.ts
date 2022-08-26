d8f219e0d5cabf3b54a88ab7211660248b06b575
Leonardo José Couto Silva Lopes de Souza <leonardojcsl@gmail.com>
2022-08-26 18:18:29 -0300

    alterações1-to-do-list

--- modulo4-alter/to-do-list/src/index.ts
+++ modulo4-alter/to-do-list/src/index.ts
@@ -6,16 +6,16 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 
-export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: "35.226.146.116",
-    port: 3306,
-    user: "alves-leonardo-souza",
-    password: "TLDxrLwzLk9VRgQHMllK",
-    database: "alves-leonardo-souza"
-  }
-});
+// export const connection = knex({
+// 	client: "mysql",
+// 	connection: {
+//     host: "35.226.146.116",
+//     port: 3306,
+//     user: "alves-leonardo-souza",
+//     password: "TLDxrLwzLk9VRgQHMllK",
+//     database: "alves-leonardo-souza"
+//   }
+// });
 
 app.use(express.json())
 app.use(cors())
